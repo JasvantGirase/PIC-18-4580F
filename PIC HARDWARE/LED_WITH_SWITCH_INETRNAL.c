43ce0d970d0c8efb4828d5caf2caee3f2c5bbcf1
JasvantGirase <girasejasvant22@gmail.com>
2026-09-10 00:20:41 +0530

    Create LED_WITH_SWITCH_INETRNAL.c

---
+++ PIC HARDWARE/LED_WITH_SWITCH_INETRNAL.c
@@ -0,0 +1,22 @@
+#include <p18f4580.h>
+
+#define LED PORTBbits.RB1
+#define SW  PORTCbits.RC3
+
+void main(void)
+{
+    TRISBbits.RB1 = 0;    // RB1 as output
+    TRISCbits.RC3 = 1;    // RC3 as input
+
+    while(1)
+    {
+        if(SW == 0)
+        {
+            LED = 1;
+        }
+        else
+        {
+            LED = 0;
+        }
+    }
+}
